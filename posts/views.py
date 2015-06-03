7340bde78c9075bed7491867c903cdf45a0cb77d
mincaeuro <mincaeuro@gmail.com>
2015-06-03 15:03:34 +0200

    update_15-06-03-15-03-34

--- posts/views.py
+++ posts/views.py
@@ -10,15 +10,15 @@ def index(request):
 	context = {'posledne_prispevky': posledne_prispevky}
 	return render(request, 'posts/index.html', context)
 
-#def detail(request, post_id):
-#	prispevok = get_object_or_404(Prispevok, pk=post_id)
-#	return render(request, 'posts/detail.html', {'prispevok': prispevok})
-
 def detail(request, post_id):
-    prispevok = Prispevok.objects.all().order_by('pub_date')
-    komentar = Komentar.objects.all().order_by('pub_date')
-    context = {'prispevok': prispevok,'komentar': komentar}
-    return render(request, 'posts/detail.html', context)
+	prispevok = get_object_or_404(Prispevok, pk=post_id)
+	return render(request, 'posts/detail.html', {'prispevok': prispevok})
+
+#def detail(request, post_id):
+#    prispevok = Prispevok.objects.all().order_by('pub_date')
+#    komentar = Komentar.objects.all().order_by('pub_date')
+#    context = {'prispevok': Prispevok,'komentar': komentar}
+#    return render(request, 'posts/detail.html', context)
 
 	
 
