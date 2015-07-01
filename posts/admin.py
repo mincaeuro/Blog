11e66042dc21f9bf0650690b138251d04f1a5dc1
mincaeuro <mincaeuro@gmail.com>
2015-07-01 17:31:56 +0200

    update_15-07-01-17-31-56

--- posts/admin.py
+++ posts/admin.py
@@ -2,24 +2,24 @@ from django.contrib import admin
 
 from .models import Komentar, Prispevok
 
-# Register your models here.
 
-#class KomentPodPrispevkom(admin.StackedInline):
-#	model = Prispevok
-#	extra = 3
-
-#class KomentAdmin(admin.ModelAdmin):
-#	fieldsets = [
-#	(None, {'fields': ['text']}),
-#	('Date info', {'fileds':['pub_date'], 'classes': ['collapse']}),
-#	]
-#	inlines = [KomentPodPrispevkom]
-
+class KomentPodPrispevkom(admin.StackedInline):
+    model = Komentar
+    extra = 0
 
 class PrispevokAdm(admin.ModelAdmin):
-	list_display = ('title', 'pub_date','user', 'was_published_recently')
-	list_filter = ['pub_date']
-	search_fields = ['title']
+	#list_display = ('title', 'pub_date','user', 'was_published_recently')
+	fieldsets = [
+        (None,               {'fields': ['title']}),
+        (None,               {'fields': ['text']}),
+        (None,               {'fields': ['image']}),
+	(None,			{'fields': ['user']}),
+        (None,			 {'fields': ['pub_date']}),
+	]
+	inlines = [KomentPodPrispevkom]
+    #list_filter = ['pub_date']
+    #search_fields = ['title']
+	
 
 class KomentAdm(admin.ModelAdmin):
 	list_display = ('text', 'pub_date','user', 'was_published_recently', 'postRef')
